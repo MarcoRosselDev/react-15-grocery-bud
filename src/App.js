579798d554f4de63593046b7cd1520ac861650f2
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-09-25 13:04:59 -0300

    use state + structure return 2

--- src/App.js
+++ src/App.js
@@ -17,11 +17,17 @@ function App() {
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
-        <div className="grocery-container">
-          <List />
-          <button className="clear-btn">clear items</button>
+        {alert.show && <Alert />}
+        <h3>grocery bud</h3>
+        <div className="form-control">
+          <input type="text" />
+          <button type="submit" className="submit-btn"></button>
         </div>
       </form>
+      <div className="grocery-container">
+        <List />
+        <button className="clear-btn">clear items</button>
+      </div>
     </section>
   );
 }
